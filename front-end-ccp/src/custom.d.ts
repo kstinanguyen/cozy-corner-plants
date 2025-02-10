de8f9cb6c80ad50016898d4be7060752df158a69
Kristina N <87397396+kstinanguyen@users.noreply.github.com>
2025-02-10 14:30:19 -0800

    updated monorepo

--- front-end-ccp/src/custom.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg' {
-  import React from 'react';
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
--- front-end-ccp/src/utils/plantData.ts
+++
@@ -1,5 +0,0 @@
-export const plantPotColors: Record<number, string[]> = {
-  1: ["brown", "red", "blue"],
-  2: ["green", "purple"],
-  3: ["clay", "cobalt", "white"],
-};
